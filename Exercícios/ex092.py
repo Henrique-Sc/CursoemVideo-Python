1b67fa4b264a54985d06c9dd992855e099578039
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-03-07 09:56:07 -0300

    Update ex092.py

--- Exercícios/ex092.py
+++ Exercícios/ex092.py
@@ -3,16 +3,19 @@ from datetime import datetime
 from time import sleep as s
 
 # Pegando dados iniciais
-user = {
-    'Nome': str(input('Nome: ')).strip().upper(),
-    'Idade': datetime.now().year - int(input('Ano de nascimento: ').strip()),
-    'Ctps': int(input('Número da Cateira de Trabalho (Se não tiver, digite 0): '))}
+user = dict()
+user['Nome'] = str(input('Nome: ')).strip().upper()
+nasc = int(input('Ano de nascimento: ').strip())
+user['Idade']= datetime.now().year - nasc
+user['Ctps'] = int(input('Número da Cateira de Trabalho (Se não tiver, digite 0): '))
 
+# Se tiver carteira de trabalho, terá que inserir mais dados
 if user['Ctps'] != 0:
     user['Ano de contratação'] = int(input('Ano de contratação: ').strip())
     user['Salário'] = f'{int(input("Salário: R$")):.2f}'
-    user['Data da aposentadoria'] = user['Ano de contratação'] + 35
+    user['Idade da aposentadoria'] = (user['Ano de contratação'] + 35) - nasc
 
+# Mostrando os dados
 s(0.5)
 print('\n=-=-=-=-=-=-=-=-=-=-=-=-=\n')
 for k, v in user.items():
