727426d3020d00f358a6b434c7a755f0dbc1d837
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-03-10 20:47:46 -0300

    Update Aula 20 - Funções a.py

--- Aulas/Aula 20 - Funções a.py
+++ Aulas/Aula 20 - Funções a.py
@@ -1,10 +1,8 @@
-def linha():
-    print('-' * 30)
 
 
-linha()
 print(f'{"CURSO EM VÍDEO":^30}')
-print('-' * 30)
-print('-' * 30)
+
 print(f'{"Henrique da Silva Costa":^30}')
-print('-' * 30)
+
+print(f'{"Textinho feliz :)":^30}')
+
