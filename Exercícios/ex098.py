a55cbe00145f3d560da823c32bf3759fd22cf137
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-03-11 12:07:45 -0300

    update ex098.py

---
+++ Exercícios/ex098.py
@@ -0,0 +1,11 @@
+# Fazer um contador
+
+# Funcionalidades necessárias:
+#   > Mostrar uma contagem de 1 até 10 de 1 em 1
+#   > Mostrar uma contagem de 10 até 0 de 2 em 2
+#   > Mostrar uma contagem personalizada, escolhida pelo usuário
+#       • Funções necessárias:
+#            > Funcionar de trás para frente (90 até 40, de 10 em 10)
+#            > Se o passo for negativo ou zero, deve ser considerado com 1
+
+def contador(inicio, fim, passo):
