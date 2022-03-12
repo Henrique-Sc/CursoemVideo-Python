5d848ca5cae4290d46da3c96f624a55c8da59403
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-03-12 11:29:16 -0300

    update ex099.py

--- Exercícios/ex099.py
+++ Exercícios/ex099.py
@@ -34,9 +34,26 @@ def maior(*numeros):
     sleep(0.5)
 
 
-lista = list()
-lista.append(float(input('Digite o primeiro número: ').strip()))
 while True:
+    lista = list()
+    lista.append(float(input('Digite o primeiro número: ').strip()))
+    while True:
+        esc = str(input('Deseja continuar? [Sim / Não] ')).strip().upper()[0]
+        c = 0
+        print()
+        while esc not in 'SN':
+            esc = str(input(f'Valor inválido! Digite "Sim" ou "Não": ')).strip().upper()[0]
+            c += 1
+        if c != 0:
+            print()
+
+        if esc == 'N':
+            break
+        else:
+            pass
+        lista.append(float(input('Digite outro número: ').strip()))
+    maior(*lista)
+
     esc = str(input('Deseja continuar? [Sim / Não] ')).strip().upper()[0]
     c = 0
     print()
@@ -50,7 +67,4 @@ while True:
         break
     else:
         pass
-    lista.append(float(input('Digite outro número: ').strip()))
-maior(*lista)
-
 print('<- Fim da execução :) ->')
