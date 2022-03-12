7df39834eab0e7c63de938a4c8bc51cb89cde27d
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-03-12 10:56:52 -0300

    update ex099.py

--- Exercícios/ex099.py
+++ Exercícios/ex099.py
@@ -3,10 +3,21 @@ from time import sleep
 
 def maior(*numeros):
     print('-=' * 17 + '-')
-    print('Analisando', end='')
+    sleep(0.5)
+    print('Analisando os valores inseridos', end='')
     for c in '...':
-        sleep(0.3)
+        sleep(0.5)
         print(c, end='')
-    print(c for '...')
+    sleep(0.5)
 
-maior(1)
+    print(f'\n  -> ', end='')
+    print(*numeros, sep=' - ')
+    sleep(1)
+
+    if len(numeros) == 1:
+        print(f'• Foi informado apenas {len(numeros)} número.')
+    else:
+        print(f'• Foram informados {len(numeros)} números.')
+    sleep(0.5)
+    print(f'• O maior número é o {max(numeros)}')
+
