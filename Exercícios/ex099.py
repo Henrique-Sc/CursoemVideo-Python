c69dc0893630638be55743aa604a75f3f61793cb
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-03-12 10:43:50 -0300

    update ex098.py

--- Exercícios/ex099.py
+++ Exercícios/ex099.py
@@ -1,2 +1,13 @@
 from time import sleep
 
+
+def maior(*numeros):
+    print('-=' * 17 + '-')
+    print('Analisando', end='')
+    for c in '...':
+        sleep(0.3)
+        print(c, end='')
+    print(c for '...')
+
+
+maior(1)
