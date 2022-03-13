d68d94b1dd1235e3f97abcc564869126597d8393
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-03-13 10:56:53 -0300

    update ex097.py

--- Exercícios/ex098.py
+++ Exercícios/ex098.py
@@ -1,8 +1,8 @@
 from time import sleep
 
-# Determinando a função
 
-def contador(inicio, fim, passo):
+# Determinando a função
+def contador(inicio, fim, passo, timer=0.3):
     # Verificação dos dados, para trata-lo corretamente
     if passo == 0:
         passo = 1
@@ -12,9 +12,9 @@ def contador(inicio, fim, passo):
     # Exibição dos dados
     # Linha bonita :)
     print('-' * 50)
-    sleep(0.3)
+    sleep(timer)
     print(f'Contagem de {inicio} até {fim} de {passo} em {passo}!')
-    sleep(0.3)
+    sleep(timer)
 
     # Verificação dos dados, para trata-lo corretamente
     if fim > inicio:
@@ -26,20 +26,19 @@ def contador(inicio, fim, passo):
     print('   -> ', end='')
     for c in range(inicio, fim, passo):
         print(f'{c}', end=' ')
-        sleep(0.21)
+        sleep(timer)
     print('FIM!')
-    sleep(0.3)
+    sleep(timer)
     # Linha bonita :)
     print('-' * 50)
-    sleep(0.3)
+    sleep(timer)
 
 
 contador(1, 10, 1)
 contador(10, 0, 2)
 
-print('\n' + '-' * 50)
-print('Agora é a sua vez de fazer uma contagem personalizada :)')
-inicio = int(input('Início: '))
-fim = int(input('Fim: '))
-passo = int(input('Passo: '))
-contador(inicio, fim, passo)
+print('\nAgora é a sua vez de fazer uma contagem personalizada :)')
+i = int(input('Início: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+contador(i, f, p)
