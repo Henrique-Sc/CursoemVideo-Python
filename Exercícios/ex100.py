2a379dbb342af2081b06ca9014cda6566558d35e
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-03-13 09:26:12 -0300

    update ex100.py

--- Exercícios/ex100.py
+++ Exercícios/ex100.py
@@ -1,19 +1,26 @@
 from random import randint
+from time import sleep
 
 
-def sorteia():
-    numeros = []
-    for c in range(5):
-        numeros.append(randint(1, 10))
-    print(f'Os valores')
-    return numeros
+def sorteio(nums, tot_sort=5):
+    print(f'Sorteando {tot_sort} números: ', end='')
+
+    for c in range(tot_sort):
+        r = randint(1, 10)
+        nums.append(r)
+        print(r, end=' ', sep=f'{sleep(0.5)}')
+    print()
 
 
-def somarPar(numeros):
-    tot_par = 0
-    for num in numeros:
+def somarPar(nums):
+    tot_par = list()
+    for num in nums:
         if num % 2 == 0:
-            tot_par += num
-    print(tot_par)
+            tot_par.append(num)
+    print(f'A soma numeros PARES é: ', end=''), sleep(0.5)
+    print(sum(tot_par))
 
 
+numeros = list()
+sorteio(numeros)
+somarPar(numeros)
