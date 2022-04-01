ec26f2797475687bf774796548542987810050be
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-04-01 09:48:27 -0300

    Update Aula 21 - Funções (parte 2) a.py

--- Aulas/Aula 21 - Funções (parte 2) a.py
+++ Aulas/Aula 21 - Funções (parte 2) a.py
@@ -1,1 +1,20 @@
+# Interactive Help no Python
 print(input.__doc__)
+help(input)
+
+
+def contador1(i, f, p):
+    """
+    -> Faz uma contagem e mostra na tela.
+    :param i: Início da contagem
+    :param f: Final da contagem
+    :param p: Passo da contagem
+    :return: sem retorno
+    """
+
+    for c in range(i, f + 1, p):
+        print(c, end=' → ')
+    print('FIM')
+
+
+contador1(1, 10, 1)
