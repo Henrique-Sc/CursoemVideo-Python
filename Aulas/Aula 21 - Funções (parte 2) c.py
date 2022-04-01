ae9357c6087bbad4675ac74b71cfbbdc6df170c5
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-04-01 10:21:09 -0300

    Update "Aula21 - Funções (parte 2) c.py"

---
+++ Aulas/Aula 21 - Funções (parte 2) c.py
@@ -0,0 +1,11 @@
+def teste():
+    x = 8
+    print(f'Na função teste, n vale {n}')
+    print(f'na função teste, x vale x')
+
+
+# Programa principal
+n = 2
+print(f'No programa principal, n vale {n}')
+teste()
+print(f'NO programa principal, x vale {x}')
