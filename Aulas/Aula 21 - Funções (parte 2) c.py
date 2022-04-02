c0ecde0403b16a77e22d392c1dbb4eb23e93029a
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-04-02 09:38:34 -0300

    Update "Aula21 - Funções (parte 2) c.py"

--- Aulas/Aula 21 - Funções (parte 2) c.py
+++ Aulas/Aula 21 - Funções (parte 2) c.py
@@ -1,11 +1,13 @@
-def teste():
-    x = 8
-    print(f'Na função teste, n vale {n}')
-    print(f'na função teste, x vale x')
+def funcao():
+    global n1, n2
+    n1 = 4
+    n2 = 10
+    print(f'N1 dentro vale {n1}')
+    print(f'N2 dentro vale {n2}')
 
 
-# Programa principal
-n = 2
-print(f'No programa principal, n vale {n}')
-teste()
-print(f'NO programa principal, x vale {x}')
+n1 = 2
+n2 = 8
+funcao()
+print(f'N1 no global vale {n1}')
+print(f'N2 no global vale {n2}')
