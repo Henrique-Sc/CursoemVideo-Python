79a905dd47b28349056cb8e2c83ab755c0fb593b
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-04-02 12:33:08 -0300

    Update ex103.py

--- Exercícios/ex103.py
+++ Exercícios/ex103.py
@@ -1,2 +1,16 @@
-a = input('Digite algo: ')
-print(f'Valor de a: {a}')
+def jogador(nome='<desconhecido>', totgols=0):
+    print(f'O jogador {nome} fez {totgols} gol(s)')
+
+
+n = input('Nome do jogador: ').strip().title()
+gols = input('Número de gols: ').strip()
+print()
+if gols.isnumeric():
+    int(gols)
+else:
+    gols = 0
+
+if n != '':
+    jogador(n, gols)
+else:
+    jogador(totgols=gols)
