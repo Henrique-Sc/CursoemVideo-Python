8ca96fc7deb52a6c23b3995dedca4b8f17a6a4c6
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-04-02 12:05:10 -0300

    Update ex102.py

--- Exercícios/ex102.py
+++ Exercícios/ex102.py
@@ -3,33 +3,35 @@ def fatorial(n=1, show=False):
     """
     -> Calcula o Fatorial de um número.
     :param n: Número que deseja-se saber o seu fatorial
-    :param show: Mostrar ou não a conta (opcional)
+    :param show: Retornar ou não a conta (opcional)
     :return: Retorna o Fatorial
     """
-    f = 1
-    for c in range(1, n + 1):
-        print(c)
 
-    # if not show:
-    #     print('Show False')
-    # else:
-    #     print('Show True')
-    # return f
+    f = 1
+    result = ''
+    for c in range(1, n + 1):
+        result += f'{c} ' + ('x ' if c < n else '= ')
+        f *= c
+    result += f'{f}'
+
+    if not show:
+        return f
+    else:
+        return result
 
 
 # Programa principal
 num = int(input('Digite um número para ver o seu fatorial: ').strip())
-# while True:
-#     esc = input('Deseja visualizar a conta? [Sim / Não]: ').strip().upper()[0]
-#     if esc not in 'SN':
-#         print('\nErro! Digite "S" ou "N"')
-#     else:
-#         break
-#
-# if esc == 'S':
-#     esc = True
-# else:
-#     esc = False
+while True:
+    esc = input('Deseja visualizar a conta? [Sim / Não]: ').strip().upper()[0]
+    if esc not in 'SN':
+        print('\nErro! Digite "S" ou "N"')
+    else:
+        break
 
-# print(fatorial(num, esc))
-print(fatorial(num))
+if esc == 'S':
+    esc = True
+else:
+    esc = False
+
+print(fatorial(num, esc))
