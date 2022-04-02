869e6c14fd2535190c2cc23357271b0df7f973e6
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-04-02 16:41:34 -0300

    Update ex105.py

--- Exercícios/ex105.py
+++ Exercícios/ex105.py
@@ -5,13 +5,22 @@ def notas(*n, sit=False):
         'Menor nota': min(n),
         'Média': sum(n) / len(n)
     }
+    if sit:
+        media = dic['Média']
+        if media < 5:
+            s = 'RUIM'
+        elif 5 <= media < 7:
+            s = 'RAZOÁVEL'
+        else:
+            s = 'BOA'
+        dic['Situação'] = s
     return dic
 
 
 # Programa principal
-resultado = notas(9.51, 10, 5.50, 4)
-for k, v in resultado.items():
-    if type(v) == float:
-        print(f'{k}: {v:.2f}')
+resultado = notas(8, 7, 5, 9.85)
+for i, d in enumerate(resultado.items()):
+    if i >= 1 and d[0] != 'Situação':
+        print(f'{d[0]}: {d[1]:.2f}')
     else:
-        print(f'{k}: {v}')
+        print(f'{d[0]}: {d[1]:}')
