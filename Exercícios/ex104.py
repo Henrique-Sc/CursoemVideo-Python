c65f8d475ef41fa17ec876f35384b4f4e1940aed
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-04-02 16:11:44 -0300

    Create ex104.py

--- Exercícios/ex104.py
+++ Exercícios/ex104.py
@@ -1,8 +1,12 @@
 def leiaInt(txt=''):
-    a = input(txt)
-    if a.strip().isnumeric():
-        return int(a)
+    while True:
+        num = input(txt)
+        if num.strip().isnumeric():
+            return int(num)
+        else:
+            print('\033[31mValor incorreto! Digite um número inteiro.\033[m')
 
 
+# Programa principal
 n = leiaInt('Digite um número: ')
-print(type(n))
+print(f'O número digitado foi {n}')
