cf94e41063c1a8420d84955ccaf9d2ef6adf8afc
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-04-02 10:01:19 -0300

    Update "Aula21 - Funções (parte 2) d.py"

--- Aulas/Aula 21 - Funções (parte 2) d.py
+++ Aulas/Aula 21 - Funções (parte 2) d.py
@@ -1,9 +1,11 @@
 def somar(*a):
     s = sum(a)
     # print(f'A soma vale {s}')
+    return s
 
 
-somar(1, 2, 3)
-somar(10, 5)
-somar(4)
+r1 = somar(1, 2, 3)
+r2 = somar(10, 5)
+r3 = somar(4)
 
+print(f'Os resultados foram {r1}, {r2} e {r3}')
