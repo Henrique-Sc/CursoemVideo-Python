af0130972ff2bc7de50abe0aa591be43364fef76
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-04-02 10:40:43 -0300

    Update ex101.py

--- Exercícios/ex101.py
+++ Exercícios/ex101.py
@@ -5,8 +5,17 @@ anoAtual = datetime.now().year
 
 def voto(nascimento):
     idade = anoAtual - nascimento
-    print(idade)
+
+    if idade < 16:
+        result = 'VOTO NEGADO'
+    elif 16 <= idade < 18:
+        result = 'VOTO OPCIONAL'
+    else:
+        result = 'VOTO OBRIGATÓRIO'
+
+    return f'Com {idade} anos: {result}'
 
 
 print('-' * 30)
 nasc = int(input(f'Ano de nascimento (aaaa): '))
+print(voto(nasc))
