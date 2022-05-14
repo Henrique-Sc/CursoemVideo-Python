9dbf62b38e076a168f8b3cde978cf45ff17cc28c
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-05-14 18:27:45 -0300

    Update ex106.py

--- Exercícios/ex106.py
+++ Exercícios/ex106.py
@@ -1,10 +1,10 @@
-cores = ('\033[m',         # 0 - sem cor
-         '\033[38;5;9m',   # 1 - vermelho
-         '\033[38;5;10m',  # 2 - verde
-         '\033[38;5;11m',  # 3 - amarelo
-         '\033[38;5;12m',  # 4 - azul
-         '\033[38;5;13m',
-         '\033[38;5;14m',)
+cores = ('\033[m',          # 0 - sem cor
+         '\033[38;5;9m',    # 1 - vermelho
+         '\033[38;5;10m',   # 2 - verde
+         '\033[38;5;11m',   # 3 - amarelo
+         '\033[38;5;12m',   # 4 - azul
+         '\033[38;5;13m',   # 5 - magenta
+         '\033[38;5;14m',)  # 6 - ciano
 
 
 def title(txt, cor=0):
@@ -16,8 +16,13 @@ def title(txt, cor=0):
     print(cores[0])
 
 
+def ajuda(comando):
+    help(comando)
+
+
+
 while True:
-    title('SISTEMA DE AJUDA PyHelp')
+    title('SISTEMA DE AJUDA PyHelp', cor=4)
     print(f'\033[3m~Digite fim para parar \033[m')
     func = str(input(f'{cores[1]}Função ou biblioteca > {cores[0]}')).lower()
 
@@ -25,5 +30,7 @@ while True:
 
     if func == 'fim':
         break
+    else:
+        ajuda(func)
 
-title('ATÉ LOGO!', cor=2)
+title('ATÉ LOGO!', cor=6)
