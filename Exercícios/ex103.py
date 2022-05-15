a206c3af7b0aa8ea7496c8c757ba1cb4579f775f
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-05-15 20:44:44 -0300

    Update ex103.py

--- Exercícios/ex103.py
+++ Exercícios/ex103.py
@@ -2,11 +2,15 @@ def jogador(nome='<desconhecido>', gols=0):
     print(f'O jogador {nome} fez {gols} gol(s)')
 
 
-name = str(input('Digite o nome do jogador: ')).strip().title()
+n = str(input('Digite o nome do jogador: ')).strip().title()
 g = input('Total de gols: ')
 
 if g.isnumeric():
-    int(g)
+    g = int(g)
+else:
+    g = 0
 
-# if name == '':
-
+if n == '':
+    jogador(gols=g)
+else:
+    jogador(nome=n, gols=g)
