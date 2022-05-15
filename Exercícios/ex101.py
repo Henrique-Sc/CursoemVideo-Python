da503fcb5374e868f9241df352d4c4ccbc540a87
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-05-15 17:57:12 -0300

    Update ex106.py

--- Exercícios/ex101.py
+++ Exercícios/ex101.py
@@ -4,9 +4,9 @@ def voto(nascimento):
     anoAtual = datetime.now().year
     idade = anoAtual - nascimento
 
-    if idade < 16 or idade > 60:
+    if idade < 16:
         result = 'VOTO NEGADO'
-    elif 16 <= idade < 18:
+    elif 16 <= idade < 18 or idade > 65:
         result = 'VOTO OPCIONAL'
     else:
         result = 'VOTO OBRIGATÓRIO'
