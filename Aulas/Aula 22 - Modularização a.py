9212245b2a59762a26826b16e896a6b3a16b4a66
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-05-27 16:15:40 -0300

    Update Aula 22 - Modularização a.py

---
+++ Aulas/Aula 22 - Modularização a.py
@@ -0,0 +1,7 @@
+from uteis import numeros
+
+num = int(input('Digite um valor: '))
+
+print(f'\nO fatorial de {num} é {numeros.fatorial(num)}')
+print(f'O dobro de {num} é {numeros.dobro(num)}')
+print(f'O dobro de {num} é {numeros.triplo(num)}')
