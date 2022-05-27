f43a2c81b69205da53ed495a4d0ac47d999eb683
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-05-27 18:13:40 -0300

    Updade ex110/moedas.py

---
+++ Exercícios/ex110/moedas.py
@@ -0,0 +1,47 @@
+def moeda(n):
+    n_format = f'R$ {n:,.2f}'.replace(',', '_').replace('.', ',').replace('_', '.')
+    return n_format
+
+
+def aumentar(n, x, formatar=False):
+    new_n = n + (n / 100 * x)
+
+    if formatar:
+        new_n = moeda(new_n)
+    return new_n
+
+
+def diminuir(n, x, formatar=False):
+    new_n = n - (n / 100 * x)
+
+    if formatar:
+        new_n = moeda(new_n)
+    return new_n
+
+
+def dobro(n, formatar=False):
+    new_n = n * 2
+
+    if formatar:
+        new_n = moeda(new_n)
+    return new_n
+
+
+def metade(n, formatar=False):
+    new_n = n / 2
+
+    if formatar:
+        new_n = moeda(new_n)
+    return new_n
+
+
+def resumo(n, aum, red):
+    linha = '=' * 32
+
+    print(linha)
+    print(f'{"RESUMO DO VALOR":^32}')
+    print(linha)
+    print(f' {"Dobro do valor"} \t{dobro(n, formatar=True)}')
+    print(f' {"Metade do valor"} \t{metade(n, formatar=True)}')
+    print(f' {aum}% de aumento \t{aumentar(n, aum, formatar=True)}')
+    print(f' {red}% de redução \t{diminuir(n, red, formatar=True)}')
