46c5710d0571e90da089ef98c3edf1b62bb2d63f
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-05-28 09:15:01 -0300

    Updade ex112/main.py

--- Exercícios/ex112/main.py
+++ Exercícios/ex112/main.py
@@ -1,8 +1,8 @@
-# Importar o módulo moeda
+# Importar o módulo moeda e dado
 from utilidadesCeV import moeda
 from utilidadesCeV import dado
 
 
 # Usar as funções aumentar, diminuir, dobro e metade
-p = dado.leiaDinheiro('Digite o preço do produto: R$')
-moeda.resumo(p, 80, 35)
+preco = dado.leiaDinheiro('Digite o preço do produto: R$')
+moeda.resumo(preco, 80, 35)
