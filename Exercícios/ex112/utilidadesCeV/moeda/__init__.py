45362d761cc056649bed68942274b838350f29d1
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-05-28 09:14:20 -0300

    Updade ex112/utilidadeCeV/moeda

--- Exercícios/ex112/utilidadesCeV/moeda/__init__.py
+++ Exercícios/ex112/utilidadesCeV/moeda/__init__.py
@@ -1,9 +1,23 @@
 def moeda(n):
+    """
+    -> Formata o valor recebido como monetário
+
+    :param n:  Número a ser analisado
+    :return:   Retorna o valor formatado
+    """
     n_format = f'R$ {n:,.2f}'.replace(',', '_').replace('.', ',').replace('_', '.')
     return n_format
 
 
 def aumentar(n, x, formatar=False):
+    """
+    -> Calcula o aumento de um número (em porcentagem)
+
+    :param n:         número a ser aumentado
+    :param x:         valor do aumento (%)
+    :param formatar:  parâmetro opcional, se True ele retorna o valor formatado como monetário
+    :return:          retorna o valor
+    """
     new_n = n + (n / 100 * x)
 
     if formatar:
@@ -12,6 +26,14 @@ def aumentar(n, x, formatar=False):
 
 
 def diminuir(n, x, formatar=False):
+    """
+    -> Calcula a redução de um número (em porcentagem)
+
+    :param n:         número a ser reduzido
+    :param x:         valor da redução (%)
+    :param formatar:  parâmetro opcional, se True ele retorna o valor formatado como monetário
+    :return:          retorna o valor
+    """
     new_n = n - (n / 100 * x)
 
     if formatar:
@@ -20,6 +42,13 @@ def diminuir(n, x, formatar=False):
 
 
 def dobro(n, formatar=False):
+    """
+    -> Retorna o dobro do valor inserido
+
+    :param n:         número a ser calculado o dobro
+    :param formatar:  parâmetro opcional. Se True, retorna o valor formatado como monetário
+    :return:          retorna o dobro do valor inserido
+    """
     new_n = n * 2
 
     if formatar:
@@ -28,6 +57,13 @@ def dobro(n, formatar=False):
 
 
 def metade(n, formatar=False):
+    """
+    -> Retorna a metade do valor inserido
+
+    :param n:         número a ser calculado a metade
+    :param formatar:  parâmetro opcional. Se True, retorna o valor formatado como monetário
+    :return:          retorna a metade do valor inserido
+    """
     new_n = n / 2
 
     if formatar:
@@ -36,6 +72,14 @@ def metade(n, formatar=False):
 
 
 def resumo(n, aum, red):
+    """
+    -> Mostra para o usuário uma tabulação, com todos os cálculos do módulo. Dobro, metade, aumento e redução.
+
+    :param n:    preço do produto a ser analisado
+    :param aum:  porcentagem do aumento
+    :param red:  porcentagem da redução
+    :return:     sem retorno.
+    """
     linha = '=' * 32
 
     print(linha)
