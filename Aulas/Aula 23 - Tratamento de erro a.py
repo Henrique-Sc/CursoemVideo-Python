12dc2f3288ed4d123b6d1de73d16df22e8e9db4a
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-06-30 22:46:42 -0300

    Update 'Aula 23 - Tratamento de erro a.py'

--- Aulas/Aula 23 - Tratamento de erro a.py
+++ Aulas/Aula 23 - Tratamento de erro a.py
@@ -1,5 +1,30 @@
-a = int(input('Digite um número: ').strip())
-b = int(input('Digite outro número: ').strip())
-r = a / b
+# Tente...
+try:
+    a = int(input('Digite um número: ').strip())
+    b = int(input('Digite outro número: ').strip())
+    r = a / b
 
-print(f'Resultado: {a} / {b} = {r}')
+# Análise de exceções
+# 1 - TypeError e ValueError
+except (TypeError, ValueError):
+    print(f'Tivemos um problema com o tipo de dado que você inseriu.')
+
+# 2 - KeyboardInterrupt
+except KeyboardInterrupt:
+    print('O usuário preferiu não digitar nada :(')
+
+# 3 = ZeroDivisionError
+except ZeroDivisionError:
+    print('Não é possível dividir um número por 0!')
+
+# 4 - Outras exceções
+except Exception as erro:
+    print(f'O erro encontrado foi: {erro}')
+
+# Se não der erro
+else:
+    print(f'Resultado: {a} / {b} = {r}')
+
+# Se der erro ou não, executa essa parte ↓
+finally:
+    print('\nVolte sempre :)')
