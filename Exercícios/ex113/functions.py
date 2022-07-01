4ba46a008d32f31fc0c02cd2ca4741aa5ddf157a
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-06-30 23:07:27 -0300

    Update ex113/functions.py

--- Exercícios/ex113/functions.py
+++ Exercícios/ex113/functions.py
@@ -1,6 +1,9 @@
 IRed = '\033[38;5;9m'
 reset = '\033[m'
 
+txtKeyboardError = f'{IRed}Erro! Você interrompeu o programa.{reset}'
+txtError = f'{IRed}Erro! Digite um número inteiro válido.{reset}'
+
 
 def leiaInt(txt):
     while True:
@@ -11,7 +14,23 @@ def leiaInt(txt):
             print(f'{IRed}Erro! Você interrompeu o programa.{reset}')
 
         except:
-            print(f'{IRed}Erro! Digite um número inteiro válido.{reset}')
+            print(txtError)
 
         else:
             return a
+
+
+def leiaFloat(txt):
+    while True:
+        try:
+            a = float(input(txt))
+
+        except KeyboardInterrupt:
+            print(txtKeyboardError)
+
+        except:
+            print(txtError)
+
+        else:
+            return a
+
