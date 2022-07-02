85b56dab687d09d5f51044bf9cbdaac38016e3c8
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-07-02 13:58:23 -0300

    Update ex114/functions.py

--- Exercícios/ex114/functions.py
+++ Exercícios/ex114/functions.py
@@ -1,2 +1,15 @@
+from selenium import webdriver
+
+red = '\033[31m'
+yellow = '\033[33m'
+reset = '\033[34m'
+
+
 def conectarSite(site):
-    print()
+    navegador = webdriver.Chrome()
+    try:
+        navegador.get(site)
+    except:
+        print(f'{red}Não consegui conectar no site \'{site}\'!{reset}')
+    else:
+        print(f'{yellow}Eu consegui conectar no site \'{site}\'!{reset}')
