e4d2e9183b966258bfdfeebd51ec10c28e2a21e5
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-07-13 11:12:20 -0300

    Update Exercicios/ex114/main.py

--- Exercicios/ex114/main.py
+++ Exercicios/ex114/main.py
@@ -1,5 +1,14 @@
 from functions import conectarSite
 
-nomeSite = 'https://' + str(input('Digite a url de um site: https://')).strip()
+red = '\033[31m'
+blue = '\033[34m'
+reset = '\033[m'
+
+url = 'https://' + str(input('Digite a url de um site: https://')).strip()
 print('')
-conectarSite(nomeSite)
+
+nomeSite = url.replace('https://', '').replace('www.', '')
+if conectarSite(url):
+    print(f'{blue}O site \'{nomeSite}\' está acessível no momento.{reset}')
+else:
+    print(f'{red}O site \'{nomeSite}\' não está acessível no momento.{reset}')
